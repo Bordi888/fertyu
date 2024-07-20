87f22060d55a17d15cd5494c45bd5aec406a3e56
Bordi <142247998+Bordi888@users.noreply.github.com>
2024-07-20 18:11:27 +0300

    Add files via upload

---
+++ src/pages/agreement.tsx
@@ -0,0 +1,107 @@
+import type { Component } from 'solid-js';
+
+const Agreement: Component = () => {
+    return (
+        <div style="background-color: black">
+            <div style="padding: 30px; padding-bottom: 50px">
+                <h2>Kullanıcı Sözleşmesi</h2>
+                <p>Son güncellenme: 06/07/2023</p>
+
+                <div style="text-align: left">
+                    <h2>Kullanım Koşulları</h2>
+                    <p>Sevgili ziyaretçimiz, lütfen https://sehirli.online web sitemizi ziyaret etmeden önce işbu kullanım koşulları sözleşmesini dikkatlice okuyunuz. Siteye erişiminiz tamamen bu sözleşmeyi kabulünüze ve bu sözleşme ile belirlenen şartlara uymanıza bağlıdır. Şayet bu sözleşmede yazan herhangi bir koşulu kabul etmiyorsanız, lütfen siteye erişiminizi sonlandırınız. Siteye erişiminizi sürdürdüğünüz takdirde, koşulsuz ve kısıtlamasız olarak, işbu sözleşme metninin tamamını kabul ettiğinizin, tarafımızca varsayılacağını lütfen unutmayınız.
+                    </p>
+                    <p>https://sehirli.online/ sitesi Emir Sürmen tarafından yönetilmekte olup, bundan sonra Şehirli olarak anılacaktır. İşbu siteye ilişkin Kullanım Koşulları, yayınlanmakla yürürlüğe girer. Değişiklik yapma hakkı, tek taraflı olarak Şehirli'ye aittir ve Şehirli üzerinden güncel olarak paylaşılacak olan bu değişiklikleri, tüm kullanıcılarımız baştan kabul etmiş sayılır.
+                    </p>
+
+
+                    <h3>Gizlilik</h3>
+                    <p>Gizlilik, ayrı bir sayfada, kişisel verilerinizin tarafımızca işlenmesinin esaslarını düzenlemek üzere mevcuttur. Şehirli'yi kullandığınız takdirde, bu verilerin işlenmesinin gizlilik politikasına uygun olarak gerçekleştiğini kabul edersiniz.
+                    </p>
+
+                    <h3>Hizmet Kapsamı</h3>
+                    <p>Şirket Adı olarak, sunacağımız hizmetlerin kapsamını ve niteliğini, yasalar çerçevesinde belirlemekte tamamen serbest olup; hizmetlere ilişkin yapacağımız değişiklikler, Şehirli'de yayınlanmakla yürürlüğe girmiş sayılacaktır.
+                    </p>
+
+                    <h3>Telif Hakları</h3>
+                    <p>Şehirli'de yayınlanan tüm metin, kod, grafikler, logolar, resimler, ses dosyaları ve kullanılan yazılımın sahibi (bundan böyle ve daha sonra "içerik" olarak anılacaktır) Şehirli olup, tüm hakları saklıdır. Yazılı izin olmaksızın site içeriğinin çoğaltılması veya kopyalanması kesinlikle yasaktır.
+                    </p>
+
+                    <h3>Genel Hükümler</h3>
+
+                    <p>
+                        Kullanıcıların tamamı, Şehirli'yi yalnızca hukuka uygun ve şahsi amaçlarla kullanacaklarını ve üçüncü kişinin haklarına tecavüz teşkil edecek nitelikteki herhangi bir faaliyette bulunmayacağını taahhüt eder. Şehirli dâhilinde yaptıkları işlem ve eylemlerindeki, hukuki ve cezai sorumlulukları kendilerine aittir. İşbu iş ve eylemler sebebiyle, üçüncü kişilerin uğradıkları veya uğrayabilecekleri zararlardan dolayı Şehirli'nin doğrudan ve/veya dolaylı hiçbir sorumluluğu yoktur.
+                        Şehirli'de mevcut bilgilerin doğruluk ve güncelliğini sağlamak için elimizden geleni yapmaktayız. Lakin gösterdiğimiz çabaya rağmen, bu bilgiler, fiili değişikliklerin gerisinde kalabilir, birtakım farklılıklar olabilir. Bu sebeple, site içerisinde yer alan bilgilerin doğruluğu ve güncelliği ile ilgili tarafımızca, açık veya zımni, herhangi bir garanti verilmemekte, hiçbir taahhütte bulunulmamaktadır.
+                        Şehirli'de üçüncü şahıslar tarafından işletilen ve içerikleri tarafımızca bilinmeyen diğer web sitelerine, uygulamalara ve platformlara köprüler (hyperlink) bulunabilir. Şehirli, işlevsellik yalnızca bu sitelere ulaşımı sağlamakta olup, içerikleri ile ilgili hiçbir sorumluluk kabul etmemekteyiz.
+                        Şehirli'yi virüslerden temizlenmiş tutmak konusunda elimizden geleni yapsak da, virüslerin tamamen bulunmadığı garantisini vermemekteyiz. Bu nedenle veri indirirken, virüslere karşı gerekli önlemi almak, kullanıcıların sorumluluğundadır. Virüs vb. kötü amaçlı programlar, kodlar veya materyallerin sebep olabileceği zararlardan dolayı sorumluluk kabul etmemekteyiz.
+                        Şehirli'de sunulan hizmetlerde, kusur veya hata olmayacağına ya da kesintisiz hizmet verileceğine dair garanti vermemekteyiz. Şehirli'ye ve sitenin hizmetlerine veya herhangi bir bölümüne olan erişiminizi önceden bildirmeksizin herhangi bir zamanda sonlandırabiliriz.
+                        Sorumluluğun Sınırlandırılması
+                        Şehirli'nin kullanımından doğan zararlara ilişkin sorumluluğumuz, kast ve ağır ihmal ile sınırlıdır. Sözleşmenin ihlalinden doğan zararlarda, talep edilebilecek toplam tazminat, öngörülebilir hasarlar ile sınırlıdır. Yukarıda bahsedilen sorumluluk sınırlamaları aynı zamanda insan hayatına, bedeni yaralanmaya veya bir kişinin sağlığına gelebilecek zararlar durumunda geçerli değildir. Hukuken mücbir sebep sayılan tüm durumlarda, gecikme, ifa etmeme veya temerrütten dolayı, herhangi bir tazminat yükümlülüğümüz doğmayacaktır.
+                    </p>
+
+                    <p>Uyuşmazlık Çözümü: İşbu Sözleşme'nin uygulanmasından veya yorumlanmasından doğacak her türlü uyuşmazlığın çözümünde, Türkiye Cumhuriyeti yasaları uygulanır; Yozgat Adliyesi Mahkemeleri ve İcra Daireleri yetkilidir.
+                    </p>
+
+                    <h1>Gizlilik Politikası</h1>
+                    <p>
+                    Güvenliğiniz bizim için önemli. Bu sebeple bizimle paylaşacağınız kişisel verileriz hassasiyetle korunmaktadır.
+                    </p>
+                    <p>
+                    Biz, Şehirli, veri sorumlusu olarak, bu gizlilik ve kişisel verilerin korunması politikası ile,
+                    hangi kişisel verilerinizin hangi amaçla işleneceği, işlenen verilerin kimlerle ve neden paylaşılabileceği,
+                    veri işleme yöntemimiz ve hukuki sebeplerimiz ile; işlenen verilerinize ilişkin haklarınızın neler
+                    olduğu hususunda sizleri aydınlatmayı amaçlıyoruz.
+                    </p>
+                    <h2>Toplanan Kişisel Verileriniz, Toplanma Yöntemi ve Hukuki Sebebi</h2>
+                    <p>
+                     Kimlik, (isim, soy isim, doğum tarihi gibi) iletişim, (adres, e-posta adresi, telefon, IP, konum gibi) özlük, sosyal medya, finans bilgileriniz ile görsel ve işitsel kayıtlarınız tarafımızca, çerezler (cookies) vb. teknolojiler vasıtasıyla, otomatik veya otomatik olmayan yöntemlerle ve bazen de analitik sağlayıcılar, reklam ağları, arama bilgi sağlayıcıları, teknoloji sağlayıcıları gibi üçüncü taraflardan elde edilerek, kaydedilerek, depolanarak ve güncellenerek, aramızdaki hizmet ve sözleşme ilişkisi çerçevesinde ve süresince, meşru menfaat işleme şartına dayanılarak işlenecektir.
+                    </p>
+                    <h2>Kişisel Verilerinizin İşlenme Amacı</h2>
+                    <p>
+                    Bizimle paylaştığınız kişisel verileriniz; hizmetlerimizden faydalanabilmeniz
+                    amacıyla sizlerle sözleşmeler kurabilmek, sunduğumuz hizmetlerin gerekliliklerini
+                    en iyi şekilde ve aramızdaki sözleşmelere uygun olarak yerine getirebilmek,
+                    bu sözleşmelerden doğan haklarınızın tarafınızca kullanılmasını sağlayabilmek,
+                    ürün ve hizmetlerimizi, ihtiyaçlarınız doğrultusunda geliştirebilmek ve bu
+                    gelişmelerden sizleri haberdar edebilmek, ayrıca sizleri daha geniş kapsamlı hizmet
+                    sağlayıcıları ile yasal çerçeveler içerisinde buluşturabilmek ve kanundan doğan
+                    zorunlulukların (kişisel verilerin talep halinde adli ve idari makamlarla paylaşılması)
+                    yerine getirilebilmesi amacıyla, sözleşme ve hizmet süresince, amacına uygun ve ölçülü bir
+                    şekilde işlenecek ve güncellenecektir.
+                    </p>
+                    <h2>Toplanan Kişisel Verilerin Kimlere ve Hangi Amaçlarla Aktarılabileceği</h2>
+                    <p>
+                    Bizimle paylaştığınız kişisel verileriniz; faaliyetlerimizi yürütmek üzere hizmet
+                    aldığımız ve/veya verdiğimiz, sözleşmesel ilişki içerisinde bulunduğumuz,
+                    iş birliği yaptığımız, yurt içi ve yurt dışındaki 3. şahıslar ile kurum ve
+                    kuruluşlara ve talep halinde adli ve idari makamlara, gerekli teknik ve idari
+                    önlemler alınması koşulu ile aktarılabilecektir.
+                    </p>
+                    <h2>Kişisel Verileri İşlenen Kişi Olarak Haklarınız</h2>
+                    <p>KVKK madde 11 uyarınca herkes, veri sorumlusuna başvurarak aşağıdaki haklarını kullanabilir:</p>
+                    <ol type="a">
+                    <li>Kişisel veri işlenip işlenmediğini öğrenme,</li>
+                    <li>Kişisel verileri işlenmişse buna ilişkin bilgi talep etme,</li>
+                    <li>Kişisel verilerin işlenme amacını ve bunların amacına uygun kullanılıp kullanılmadığını öğrenme,</li>
+                    <li>Yurt içinde veya yurt dışında kişisel verilerin aktarıldığı üçüncü kişileri bilme,</li>
+                    <li>Kişisel verilerin eksik veya yanlış işlenmiş olması hâlinde bunların düzeltilmesini isteme,</li>
+                    <li>Kişisel verilerin silinmesini veya yok edilmesini isteme,</li>
+                    <li>(e) ve (f) bentleri uyarınca yapılan işlemlerin, kişisel verilerin aktarıldığı üçüncü kişilere bildirilmesini isteme,</li>
+                    <li>İşlenen verilerin münhasıran otomatik sistemler vasıtasıyla analiz edilmesi suretiyle kişinin kendisi aleyhine bir sonucun ortaya çıkmasına itiraz etme,</li>
+                    <li>Kişisel verilerin kanuna aykırı olarak işlenmesi sebebiyle zarara uğraması hâlinde zararın giderilmesini talep etme, haklarına sahiptir.</li>
+                    </ol>
+                    <p>Yukarıda sayılan haklarınızı kullanmak üzere sehirliapp@gmail.com üzerinden bizimle iletişime geçebilirsiniz.</p>
+                    <h2>İletişim</h2>
+                    <p>
+                    Sizlere talepleriniz doğrultusunda hizmet sunabilmek amacıyla, sadece gerekli olan kişisel verilerinizin,
+                    işbu gizlilik ve kişisel verilerin işlenmesi politikası uyarınca işlenmesini, kabul edip etmemek hususunda tamamen özgürsünüz. Uygulamayı kullanmaya devam ettiğiniz takdirde, kabul etmiş olduğunuz tarafımızca varsayılacaktır. Şayet kabul etmiyorsanız, lütfen uygulamayı tüm cihazlarınızdan kaldırınız. Ayrıntılı bilgi için bizimle sehirliapp@gmail.com e-mail adresi üzerinden iletişime geçmekten lütfen çekinmeyiniz.
+                    </p>
+                    <p>Bu gizlilik politikası, <a href="https://sartlar.com/" style="color: white">https://sartlar.com</a> aracılığıyla oluşturulmuştur.</p>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Agreement
---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const Contact: Component = () => {
+    return (
+        <div style="padding: 30px">
+            <p>E-posta: sehirliapp@gmail.com</p>
+        </div>
+    );
+}
+
+export default Contact
